1311c94c26ee529d086b4981895e291472607888
sugtao4423 <sugtao4423@gmail.com>
2025-01-08 19:49:06 +0900

    Highlight whitespace

--- nvim/autoload/color.lua
+++ nvim/autoload/color.lua
@@ -1,1 +1,4 @@
 vim.api.nvim_set_hl(0, 'LineNr', { fg = '#af5f00', ctermfg = 130 })
+
+vim.api.nvim_set_hl(0, 'EoLSpace', { bg = 'darkred', ctermbg = 'darkred' })
+vim.fn.matchadd('EoLSpace', [[\(\s\|　\)\+$]])

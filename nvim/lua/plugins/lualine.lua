8e9b9ff0efe822b4e2fd3437be01f85ee0581654
sugtao4423 <sugtao4423@gmail.com>
2025-01-08 18:10:16 +0900

    Remove git branch from lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -9,16 +9,22 @@ return {
     theme.replace.a.gui = nil
     theme.inactive.a.gui = nil
 
-    theme.normal.b.bg = '#666666' -- base00
-    theme.normal.b.fg = '#a8a8a8' -- base2
-    theme.normal.c.fg = '#d0d0d0' -- base3
+    theme.normal.b = {
+      bg = '#666666', -- base00
+      fg = '#d0d0d0', -- base3
+    }
     theme.normal.x = {
       bg = '#444444', -- base02
       fg = '#a8a8a8', -- base2
     }
+    theme.normal.y = {
+      bg = '#666666', -- base00
+      fg = '#a8a8a8', -- base2
+    }
+    theme.inactive.b = theme.inactive.c
 
     local sections = {
-      lualine_c = {
+      lualine_b = {
         {
           function()
             local readonly = vim.api.nvim_get_option_value('readonly', {})
@@ -37,16 +43,18 @@ return {
           },
         },
       },
+      lualine_c = {},
     }
 
     local inactive_sections = {
-      lualine_c = {
+      lualine_b = {
         {
-          sections.lualine_c[1][1],
+          sections.lualine_b[1][1],
           color = theme.normal.x,
         },
-        sections.lualine_c[2],
+        sections.lualine_b[2],
       },
+      lualine_c = {},
     }
 
     require('lualine').setup({

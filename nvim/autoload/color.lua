93a0bdc39428d1716114801fc2902258fa7d745a
sugtao4423 <sugtao4423@gmail.com>
2025-01-12 03:03:28 +0900

    Refactor for set color

--- nvim/autoload/color.lua
+++ nvim/autoload/color.lua
@@ -1,10 +1,18 @@
-vim.api.nvim_set_hl(0, 'LineNr', { fg = '#af5f00', ctermfg = 130 })
+local colors = {
+  { 'LineNr', { fg = '#af5f00', ctermfg = 130 } },
+  { 'EoLSpace', { bg = 'darkred', ctermbg = 'darkred' } },
 
-vim.api.nvim_set_hl(0, 'EoLSpace', { bg = 'darkred', ctermbg = 'darkred' })
+  { 'CmpNormal', { bg = '#303030', ctermbg = 236, fg = '#ffffff', ctermfg = 15 } },
+  { 'CmpNormalSel', { bg = '#6c6c6c', ctermbg = 242 } },
+  { 'CmpItemAbbrDeprecated', { strikethrough = true, fg = '#808080', ctermfg = 8 } },
+  { 'CmpItemAbbrMatch', { fg = '#00ffd7', ctermfg = 50 } },
+  { 'CmpItemAbbrMatchFuzzy', { link = 'CmpItemAbbrMatch' } },
+}
+
+for _, v in ipairs(colors) do
+  local name = v[1]
+  local val = v[2]
+  vim.api.nvim_set_hl(0, name, val)
+end
+
 vim.fn.matchadd('EoLSpace', [[\(\s\|　\)\+$]])
-
-vim.api.nvim_set_hl(0, 'CmpNormal', { bg = '#303030', ctermbg = 236, fg = '#ffffff', ctermfg = 15 })
-vim.api.nvim_set_hl(0, 'CmpNormalSel', { bg = '#6c6c6c', ctermbg = 242 })
-vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { strikethrough = true, fg = '#808080', ctermfg = 8 })
-vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { fg = '#00ffd7', ctermfg = 50 })
-vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link = 'CmpItemAbbrMatch' })

87e4f6ad5bb361ffc13b793580583120284e1301
sugtao4423 <sugtao4423@gmail.com>
2025-01-13 03:21:05 +0900

    Add detection to enable nvim-treesitter

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,16 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  enabled = function()
+    local compilers = { 'cc', 'gcc', 'clang', 'cl', 'zig' }
+    for _, compiler in ipairs(compilers) do
+      if vim.fn.executable(compiler) == 1 then
+        return true
+      end
+    end
+    vim.notify('No C compiler found! nvim-treesitter is disabled.', vim.log.levels.WARN)
+    return false
+  end,
   config = function()
     require('nvim-treesitter.configs').setup({
       ensure_installed = {
